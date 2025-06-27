744c8b7ef4806b4a5f79211dce20cb4c73697f1b
rush020114 <rush020114@gmail.com>
2025-06-27 15:26:13 +0900

    Update Apple.java

--- workspace_java/Chaptor05/src/apple/Apple.java
+++ workspace_java/Chaptor05/src/apple/Apple.java
@@ -4,7 +4,7 @@ package apple;
 // 접근제한자의 종류 : private < default < protected < public
 // private : 변수, 메서드를 선언한 클래스 내에서만 접근 가능!
 // default : 같은 패키지 내에서만 접근 가능함.
-// public : 같은 프로젝트 내의 모든 곳에서 사용 가능
+// public : 같은 프로젝트 내의 모든 곳에서 사용 가능!
 // 멤버변수는 무조건 private 붙인다!
 // 메서드는 무조건 public 쓴다.
 // 지역변수에는 접근제한자를 사용 못함.

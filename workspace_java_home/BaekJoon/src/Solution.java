ebbd902b87e261680e102eac0316fca2fef2633c
rush020114 <rush020114@gmail.com>
2025-08-12 08:07:22 +0900

    1

--- workspace_java_home/BaekJoon/src/Solution.java
+++ workspace_java_home/BaekJoon/src/Solution.java
@@ -1,16 +1,73 @@
 class Solution {
-  public String[] solution(String my_str, int n) {
-    int length = my_str.length() % n == 0 ? my_str.length() / n : my_str.length() / n + 1;
-    String[] answer = new String[length];
-    for(int i = 0 ; i < answer.length ; i++){
-      if(i == answer.length - 1){
-        answer[i] = my_str.substring(i * n, i * n + my_str.length() % n);
-        break;
+  public int solution(int[] array) {
+    int index = 0;
+    for(int i = 0 ; i < array.length ; i++){
+      int cnt = 0;
+      for(int j = 0 ; j < i ; j++){
+        if(array[i] == array[j]){
+          cnt++;
+        }
       }
-      System.out.println(my_str.substring(i * n, i * n + n));
-      answer[i] = my_str.substring(i * n, i * n + n);
+      if(cnt == 0){
+        index++;
+      }
     }
-
+    int[] newArr = new int[index];
+    int[] maxes = new int[index];
+    index = 0;
+    for(int i = 0 ; i < array.length ; i++){
+      int cnt = 0;
+      for(int j = 0 ; j < i ; j++){
+        if(array[i] == array[j]){
+          cnt++;
+        }
+      }
+      if(cnt == 0){
+        newArr[index++] = array[i];
+      }
+    }
+    index = 0;
+    for(int i = 0 ; i < newArr.length ; i++){
+      int cnt = 0;
+      for(int j = 0 ; j < array.length ; j++){
+        if(newArr[i] == array[j]){
+          cnt++;
+        }
+      }
+      maxes[index++] = cnt;
+    }
+    int max = -1;
+    index = 0;
+    for(int i = 0 ; i < newArr.length ; i++){
+      if(max < maxes[i]){
+        max = maxes[i];
+        index = i;
+      }
+    }
+    for(int i = 0 ; i < newArr.length - 1 ; i++){
+      for(int j = 0 ; j < newArr.length - (i + 1) ; j++){
+        if(maxes[j] > maxes[j + 1]){
+          int temp = maxes[j];
+          maxes[j] = maxes[j + 1];
+          maxes[j + 1] = temp;
+        }
+      }
+    }
+    int answer;
+    if(array.length == 1){
+      answer = array[0];
+    }
+    else if(newArr.length == 1){
+      answer = -1;
+    }
+    else{
+      if(maxes[maxes.length - 1] == maxes[maxes.length - 2]){
+        answer = -1;
+      }
+      else{
+        answer = newArr[index];
+      }
+    }
     return answer;
   }
 }
--- workspace_java_home/BaekJoon/src/SolutionTest.java
+++ workspace_java_home/BaekJoon/src/SolutionTest.java
@@ -1,6 +1,6 @@
 public class SolutionTest {
   public static void main(String[] args) {
     Solution solution = new Solution();
-    System.out.println();
+    System.out.println(solution.solution(new int[] {1, 2, 2, 3, 3, 4, 4, 4}));
   }
 }

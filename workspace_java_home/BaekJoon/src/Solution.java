1a01a8f84a2b616acad00c19e8a3321dd47fe014
rush020114 <rush020114@gmail.com>
2025-08-13 07:41:43 +0900

    1

--- workspace_java_home/BaekJoon/src/Solution.java
+++ workspace_java_home/BaekJoon/src/Solution.java
@@ -2,23 +2,55 @@ import java.util.Arrays;
 
 class Solution {
   public int solution(int[] array, int n) {
-    int[] newArr = new int[array.length];
+    int[] newArr1 = new int[array.length];
+    int[] newArr2 = new int[array.length];
+    int[] cntArr = new int[2];
     int index = 0;
+    int result = 0;
     for(int i = 0 ; i < array.length ; i++){
-      newArr[i] = Math.abs(n - array[i]);
+      newArr1[i] = Math.abs(n - array[i]);
+      newArr2[i] = Math.abs(n - array[i]);
     }
-    System.out.println(Arrays.toString(newArr));
-    for(int i = 0 ; i < newArr.length - 1 ; i++){
-      for(int j = 0 ; j < newArr.length - (i + 1) ; j++){
-        if(newArr[j] > newArr[j + 1]){
-          int temp = newArr[j];
-          newArr[j] = newArr[j + 1];
-          newArr[j + 1] = temp;
+    for(int i = 0 ; i < newArr1.length - 1 ; i++){
+      for(int j = 0 ; j < newArr1.length - (i + 1) ; j++){
+        if(newArr2[j] > newArr2[j + 1]){
+          int temp = newArr2[j];
+          newArr2[j] = newArr2[j + 1];
+          newArr2[j + 1] = temp;
         }
       }
     }
-    System.out.println(Arrays.toString(newArr));
 
-    return array[index];
+    if(array.length != 1){
+      if(newArr2[0] == newArr2[1]){
+        for(int i = 0 ; i < newArr1.length ; i++){
+          boolean isDup = false;
+          if(newArr2[0] == newArr1[i]){
+            isDup = true;
+          }
+          if(isDup){
+            cntArr[index++] = i;
+          }
+        }
+        result = Math.min(array[cntArr[0]], array[cntArr[1]]);
+      }
+      else{
+        for(int i = 0 ; i < newArr1.length ; i++){
+          boolean isDup = false;
+          if(newArr2[0] == newArr1[i]){
+            isDup = true;
+          }
+          if(isDup){
+            index = i;
+          }
+        }
+        result = array[index];
+      }
+    }
+    else{
+      result = array[0];
+    }
+
+    return result;
   }
 }

1044318cbce9a0513b07e7822c360ff82d1407dd
rush020114 <rush020114@gmail.com>
2025-10-12 18:41:13 +0900

    백준 문제

--- workspace_java_home/BaekJoon/src/grade06/Main06_07.java
+++ workspace_java_home/BaekJoon/src/grade06/Main06_07.java
@@ -5,38 +5,25 @@ import java.util.*;
 public class Main06_07 {
   public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
-    int index = sc.nextInt();
-    String[] strings = new String[index];
-    for(int i = 0 ; i < index ; i++){
+
+    int cnt = sc.nextInt();
+    int resultCnt = 0;
+    String[] strings = new String[cnt];
+    for(int i = 0 ; i < cnt ; i++){
       strings[i] = sc.next();
     }
-
-    int resultCnt = index;
-    for(int i = 0 ; i < index ; i++){
-      Set<Character> set = new LinkedHashSet<>();
-      for(char c : strings[i].toCharArray()){
-        set.add(c);
-      }
-
-      StringBuilder sb = new StringBuilder();
-      for(Character c : set){
-        sb.append(c);
-      }
-      List<Integer> integerList = new ArrayList<>();
+    for(int i = 0 ; i < cnt ; i++){
+      boolean isGroup = true;
       for(int j = 0 ; j < strings[i].length() ; j++){
-        for(int k = 0 ; k < sb.length() ; k++){
-          if(strings[i].charAt(j) == sb.charAt(k)){
-            integerList.add(j);
-            System.out.println(strings[i].charAt(j));
-            System.out.println(sb.charAt(k));
-          }
-        }
-        if(integerList.size() >= 2){
-          if(integerList.get(1) - integerList.get(0) >= 2){
-            resultCnt--;
+        if(strings[i].indexOf(strings[i].charAt(j)) < j - 1){
+          if(strings[i].charAt(j) != strings[i].charAt(j - 1)){
+            isGroup = false;
             break;
           }
         }
+      }
+      if(isGroup){
+        resultCnt++;
       }
     }
     System.out.println(resultCnt);

9fce64e9a3d71c2a5929f3e39894ab16febfa5ed
rush020114 <rush020114@gmail.com>
2025-11-07 13:59:18 +0900

    비동기와 동기의 차이를 위한 학습

--- workspace_spring/legacy-student/src/main/java/com/green/legacy_student/mapper/StuMapper.java
+++ workspace_spring/legacy-student/src/main/java/com/green/legacy_student/mapper/StuMapper.java
@@ -1,8 +1,20 @@
 package com.green.legacy_student.mapper;
 
+import com.green.legacy_student.dto.ClassDTO;
+import com.green.legacy_student.dto.StuDTO;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface StuMapper {
+  // 학급 목록
+  List<ClassDTO> getClassList();
 
+  // 학생 목록
+  List<ClassDTO> getStuList();
+
+  List<StuDTO> getStuListAgain(int classNum);
+
+  StuDTO getStuDetail(int stuNum);
 }

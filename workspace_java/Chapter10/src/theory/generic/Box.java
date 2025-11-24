a8be1b2c5b938fee16f16abf63fbd96230797e4f
rush020114 <rush020114@gmail.com>
2025-11-24 14:53:09 +0900

    상한 제한 하한 제한 학습

--- workspace_java/Chapter10/src/theory/generic/Box.java
+++ workspace_java/Chapter10/src/theory/generic/Box.java
@@ -27,3 +27,27 @@ class Orange {
     return "I am an orange";
   }
 }
+
+class Toy {
+  public String toString(){
+    return "I am a Toy";
+  }
+}
+
+class BoxHandler{
+  // extends로 상한 제한을 둠으로써 꺼내기만 가능해졌다.
+  // 컴파일러 입장에선 Toy가 상속하는 무언가가 전달될 수도 있다는 것을 파악하고 있다.
+  // 결론적으로 Box클래스엔 Toy의 자식 클래스가 들어갈 수 있기 때문에 컴파일러가 오류를 낸다.
+  public static void outBox(Box<? extends Toy> box){
+    Toy t = box.get();
+    // box.set(new Toy());
+  }
+
+  // 마찬가지로 상한 제한을 둠으로써 넣기만 가능해졌다.
+  // 컴파일러 입장에선 Toy를 상속하는 무언가가 전달될 수도 있다는 것을 파악하고 있다.
+  // 결론적으로 Box클래스엔 Toy의 부모 클래스가 들어갈 수 있기 때문에 컴파일러가 오류를 낸다.
+  public static void inBox(Box<? super Toy> box, Toy n){
+    box.set(n);
+    // Toy myToy = box.get();
+  }
+}

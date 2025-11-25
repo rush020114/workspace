6ae1bd6488cf6ea6ede32934191f06aeed762cb2
rush020114 <rush020114@gmail.com>
2025-11-25 19:44:31 +0900

    상한 제한 하한 제한 제네릭 학습

--- workspace_java/Chapter10/src/theory/generic/Box.java
+++ workspace_java/Chapter10/src/theory/generic/Box.java
@@ -35,17 +35,21 @@ class Toy {
 }
 
 class BoxHandler{
-  // extends로 상한 제한을 둠으로써 꺼내기만 가능해졌다.
-  // 컴파일러 입장에선 Toy가 상속하는 무언가가 전달될 수도 있다는 것을 파악하고 있다.
-  // 결론적으로 Box클래스엔 Toy의 자식 클래스가 들어갈 수 있기 때문에 컴파일러가 오류를 낸다.
+  // ? extends Toy → 상한 제한 (Producer)
+  // Box 안에는 Toy 또는 Toy의 자식 타입이 들어있을 수 있다.
+  // 꺼낼 때는 최소한 Toy 타입임이 보장되므로 안전하게 꺼낼 수 있다.
+  // 하지만 넣을 때는 정확한 자식 타입을 알 수 없으므로 컴파일러가 막는다.
+  // 만약 Box<RobotToy>가 들어왔는데 new Toy()를 넣으려 하면 타입 불일치가 발생
   public static void outBox(Box<? extends Toy> box){
     Toy t = box.get();
     // box.set(new Toy());
   }
 
-  // 마찬가지로 상한 제한을 둠으로써 넣기만 가능해졌다.
-  // 컴파일러 입장에선 Toy를 상속하는 무언가가 전달될 수도 있다는 것을 파악하고 있다.
-  // 결론적으로 Box클래스엔 Toy의 부모 클래스가 들어갈 수 있기 때문에 컴파일러가 오류를 낸다.
+  // ? super Toy → 하한 제한 (Consumer)
+  // Box 안에는 Toy 또는 Toy의 부모 타입이 들어있을 수 있다.
+  // 넣을 때는 Toy 객체를 안전하게 넣을 수 있다. (부모 박스에도 자식 객체를 넣을 수 있음)
+  // 하지만 꺼낼 때는 최소 Object 타입만 보장되므로 Toy로 단정할 수 없어 컴파일러가 막는다.
+  // 예를 들어 Box<Object>일 수도 있는데, 꺼내면 Object 타입이지 Toy라고 확신할 수 없음
   public static void inBox(Box<? super Toy> box, Toy n){
     box.set(n);
     // Toy myToy = box.get();

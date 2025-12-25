4b4e6bfaf857f431fbea613f55001840da9c4a14
rush020114 <rush020114@gmail.com>
2025-12-25 21:04:21 +0900

    사람인 웹스크래퍼

--- nomad/remoteok_web_scraper.py
+++ nomad/remoteok_web_scraper.py
@@ -6,13 +6,20 @@ sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 from bs4 import BeautifulSoup
 import requests
 
-response = requests.get("https://remoteok.com/remote-golang-jobs")
+response = requests.get(
+  "https://www.saramin.co.kr/zf_user/jobs/list/job-category",
+  headers={
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36 Edg/143.0.0.0"
+  }
+)
 
+print(response.content)
+print(response.status_code)
 soup = BeautifulSoup(
   response.content,
   "html.parser"
 )
 
-jobs = soup.find("table").find_all("tr", class_="job")
+jobs = soup.find("ul", class_="list_product")
 
 print(jobs)

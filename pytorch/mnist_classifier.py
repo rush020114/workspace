4f526c463042cc54a93b1a115d884e6a1d018741
rush020114 <rush020114@gmail.com>
2026-01-15 23:09:49 +0900

    complete MNIST model training and test

--- pytorch/mnist_classifier.py
+++ pytorch/mnist_classifier.py
@@ -12,6 +12,7 @@ device = (
   else "cpu"
 )
 
+# 데이터 준비
 training_data = datasets.MNIST(
   root="data",
   train=True,
@@ -25,16 +26,74 @@ test_data = datasets.MNIST(
   download=True,
   transform=ToTensor()
 )
+
+# batch로 메모리 지키기
 train_dataloader = DataLoader(training_data, batch_size=64)
 test_dataloader = DataLoader(test_data, batch_size=64)
-for X, y in train_dataloader:
-  print(X.shape)
-  break
 
+# 예측 모델 정의
 class NeuralNetwork(nn.Module):
   def __init__(self):
     super().__init__()
     self.flatten = nn.Flatten()
     self.linear_relu_stack = nn.Sequential(
-
+      nn.Linear(28 * 28, 512),
+      nn.ReLU(),
+      nn.Linear(512, 512),
+      nn.ReLU(),
+      nn.Linear(512, 10), 
     )
+
+  def forward(self, x):
+    x = self.flatten(x)
+    logit = self.linear_relu_stack(x)
+    return logit
+  
+model = NeuralNetwork().to(device)
+
+loss_fn = nn.CrossEntropyLoss()
+batch_size = 64
+learning_rate = 1e-2
+
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+def train_loop(dataloader, model, loss_fn, optimizer):
+  model.train() # train으로 변경 (모범 사례)
+  size = len(dataloader.dataset)
+
+  for batch, (X, y) in enumerate(dataloader):
+    X, y = X.to(device), y.to(device)
+    pred = model(X) # 모델 학습으로 예측 반환
+    loss = loss_fn(pred, y) # 반환된 예측의 오차를 계산
+
+    loss.backward() # 손실에 기반한 변화도 계산
+    optimizer.step() # 변화도 기반 손실 바로 잡기
+    optimizer.zero_grad() # 변화도 초기화로 제대로 된 학습
+
+    if batch % 100 == 0:
+      loss, current = loss.item(), batch * batch_size + len(X)
+      print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+def test_loop(dataloader, model, loss_fn):
+  model.eval() # eval로 변경 (모범 사례)
+  size = len(dataloader.dataset)
+  num_batches = len(dataloader)
+  test_loss, correct = 0, 0
+
+  with torch.no_grad():
+    for X, y in dataloader:
+      X, y = X.to(device), y.to(device)
+      pred = model(X)
+      test_loss += loss_fn(pred, y).item()
+      correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+
+  test_loss /= num_batches
+  correct /= size
+  print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+epochs = 10
+for t in range(epochs):
+    print(f"Epoch {t+1}\n-------------------------------")
+    train_loop(train_dataloader, model, loss_fn, optimizer)
+    test_loop(test_dataloader, model, loss_fn)
+print("Done!")
